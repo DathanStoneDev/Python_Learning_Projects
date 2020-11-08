88eb1db02fd40788aa5798c7ed700cc389df445a
Dathan Stone <73630546+DathanStoneDev@users.noreply.github.com>
2020-11-07 22:11:54 -0700

    Add files via upload
    Continuation of notes for the basics of python

---
+++ conditionals.py
@@ -0,0 +1,42 @@
+#using the module operator and a conditional to check for even/odd nmumbers
+#number = int(input("Which number would you like to check if it is odd or even? "))
+#if number % 2 == 0:
+ #   print("Your number is even!")
+#else:
+    #print("Your number is odd!")
+
+ #Conditionals with if/else can be nested within eachother. 
+ #If there are more than 1 "else" statements, use "elif"    
+ 
+#BMI Calculator 2.0
+
+#height = float(input("Enter your height in m:"))      
+#weight = float(input("Enter your weight in kg"))   
+#bmi = round(weight / (height ** 2))
+
+#if bmi < 18.5:
+    #print(f"Your BMI is {bmi}. You are underweight")
+#elif bmi < 25:
+   # print(f"Your BMI is {bmi}. You have a normal weight")   
+#elif bmi < 30:
+    #print(f"Your BMI is {bmi}. You are overweight")
+#elif bmi < 35:
+   # print(f"Your BMI is {bmi}. You are obese")
+#else:
+   # print(f"Your BMI is {bmi}. You are clinically obese")
+   
+# Leap Year Checker
+#year = int(input("Insert the year you would like to check: "))
+
+#if year % 4 == 0:
+   # if year % 100 == 0:
+       # if year % 400 == 0:
+            #print(f"{year} is a Leap Year!")
+        #else: 
+           #print(f"{year} is not a Leap Year!")  
+    #else:
+        #print(f"{year} is a Leap Year!")
+#else:
+    #print(f"{year} is not a Leap Year!")
+
+        
